721319cbdd63bbce4f48a70dd5a5eace9045d5d2
KhurshidShod <khurshid.lenovo@gmail.com>
2025-11-29 21:53:16 -0600

    asd

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "8226618927:AAGudmbmQ0JtZjqEsdgY3-XxsFu8-xmZvWw";
 const bot = new Telegraf(TOKEN);
 const web_link = "https://burger-plus.vercel.app/";
+
 bot.start((ctx) => {
-  ctx.reply(`Assalomu aleykum`, {
+  ctx.reply("Assalomu aleykum", {
     reply_markup: {
-      inline_keyboard: [[{ text: "Bizning sayt", web_app: { url: web_link } }]],
-      resize_keyboard: true
+      keyboard: [
+        [
+          { text: "Buyurtma berish", web_app: { url: web_link } } // Web App button
+        ]
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: false,
     },
   }).then(r => console.log(r));
 });
+
 bot.launch();
